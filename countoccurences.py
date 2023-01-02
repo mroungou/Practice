81b78b516e3e9aa922c54d7c9a0701758f6fb30c
Malick Roungou <mjbroungou@gmail.com>
2023-01-02 12:40:41 -0500

    complet

--- countoccurences.py
+++ countoccurences.py
@@ -5,3 +5,17 @@
 # Given:
 
 # str_x = "Emma is good developer. Emma is a writer"
+
+sentence = "Emma is a good developer. Emma is a writer"
+
+# print(sentence.find("Emma"))
+words = sentence.split()
+occurence = 0
+for word in words:
+    if word == "Emma":
+        occurence += 1
+print(f'Emma appeared {occurence} times')
+
+# alternatively string.count() could have been used
+cnt = sentence.count("Emma")
+print(cnt)
