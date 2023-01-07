f7fd571e2c7f14dd901c83a169c7055928cd6314
Malick Roungou <mjbroungou@gmail.com>
2023-01-07 16:32:40 -0500

    complet

--- exponentcalculator.py
+++ exponentcalculator.py
@@ -2,3 +2,19 @@
 # that returns an int value of base raises to the power of exp.
 
 # Note here exp is a non-negative integer, and the base is an integer.
+# expected output
+# base = 2
+# exponent = 5
+
+# 2 raises to the power of 5: 32 i.e. (2 *2 * 2 *2 *2 = 32)
+
+def exponent(base, exp):
+    num = exp
+    result = 1
+    while num > 0:
+        result *= base
+        num -= 1
+    print(f"{base} to the power of {exp} is {result}")
+
+
+exponent(2, 3)
