2565e18a2713c72fcff7f79ed74cb03b87414a86
Malick Roungou <mjbroungou@gmail.com>
2023-01-09 13:20:54 -0500

    new exercise

---
+++ multiplication.py
@@ -0,0 +1,14 @@
+# Exercise 4: Write a program to print multiplication table of a given number
+
+# For example, num = 2 so the output should be
+
+# 2
+# 4
+# 6
+# 8
+# 10
+# 12
+# 14
+# 16
+# 18
+# 20
