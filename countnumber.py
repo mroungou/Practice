e860773f8c65a01e2bc0ea96fb5486944400d27c
Malick Roungou <mjbroungou@gmail.com>
2023-01-10 16:42:25 -0500

    complet

--- countnumber.py
+++ countnumber.py
@@ -3,3 +3,10 @@
 # Write a program to count the total number of digits in a number using a while loop.
 
 # For example, the number is 75869, so the output should be 5.
+
+number = 75869
+digit_counter = 0
+while number != 0:
+    number //= 10
+    digit_counter += 1
+print(digit_counter)
