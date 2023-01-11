8db8b0b7bb1085b610c582904533866121cd3032
Malick Roungou <mjbroungou@gmail.com>
2023-01-11 14:18:11 -0500

    complet

--- printreverselist.py
+++ printreverselist.py
@@ -2,10 +2,25 @@
 
 # Given:
 
-# list1 = [10, 20, 30, 40, 50]
+list1 = [10, 20, 30, 40, 50]
 # Expected output:
 # 50
 # 40
 # 30
 # 20
 # 10
+
+length = len(list1)
+counter = -1
+while counter >= -length:
+    print(list1[counter])
+    counter -= 1
+
+# using reversed()
+new_list = reversed(list1)
+for i in new_list:
+    print(i)
+
+# option 3
+for i in range(length - 1, -1, -1):
+    print(list1[i])
