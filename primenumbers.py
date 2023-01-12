c57833854f27b900908ede76a8da463c19ef9351
Malick Roungou <mjbroungou@gmail.com>
2023-01-12 17:18:57 -0500

    new exercise

---
+++ primenumbers.py
@@ -0,0 +1,9 @@
+# Exercise 11: Write a program to display all prime numbers within a range
+
+
+# Note: A Prime Number is a number that cannot be made by multiplying other whole numbers. A prime number is a natural number greater than 1 that is not a product of two smaller natural numbers
+
+# Examples:
+
+# 6 is not a prime mumber because it can be made by 2×3 = 6
+# 37 is a prime number because no other whole numbers multiply together to make it.
