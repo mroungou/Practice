03c3680493b4c12f0b84fe695692c8880adb2158
Malick Roungou <mjbroungou@gmail.com>
2023-01-13 11:08:07 -0500

    new exercise

---
+++ factorial.py
@@ -0,0 +1,13 @@
+# Exercise 13: Find the factorial of a given number
+
+# Write a program to use the loop to find the factorial of a given number.
+
+# The factorial (symbol: !) means to multiply all whole numbers from the chosen number down to 1.
+
+# For example: calculate the factorial of 5
+
+# 5! = 5 × 4 × 3 × 2 × 1 = 120
+# Expected output:
+
+
+# 120
