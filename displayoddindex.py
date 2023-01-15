65973c0ae867d2e120373fbe3133b6f6ac081646
Malick Roungou <mjbroungou@gmail.com>
2023-01-15 15:25:18 -0500

    new exercise

---
+++ displayoddindex.py
@@ -0,0 +1,8 @@
+# # Exercise 15: Use a loop to display elements from a given list present at odd index positions
+
+# # Given:
+
+# # my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+# Expected output:
+
+# 20 40 60 80 100
