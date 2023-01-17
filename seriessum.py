04a9aa916390cfbb29076e606fd2f0e05b6d8a54
Malick Roungou <mjbroungou@gmail.com>
2023-01-17 11:01:49 -0500

    complet

--- seriessum.py
+++ seriessum.py
@@ -1,6 +1,7 @@
 # Exercise 17: Find the sum of the series upto n terms
 
-# Write a program to calculate the sum of series up to n term. For example, if n =5 the series will become 2 + 22 + 222 + 2222 + 22222 = 24690
+# Write a program to calculate the sum of series up to n term. For example,
+#  if n =5 the series will become 2 + 22 + 222 + 2222 + 22222 = 24690
 
 # Given:
 
@@ -10,3 +11,13 @@
 
 
 # 24690
+num_iterations = 5
+num = 2
+total = 0
+
+for i in range(0, num_iterations):
+    print(num, end='+')
+    total += num
+    # calculate the next term
+    num = num * 10+2
+print(f'Sum of series is {total}')
