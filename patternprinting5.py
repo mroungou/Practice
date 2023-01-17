6fc6916e0f45ecf161fc2895741eedbdbeb2b417
Malick Roungou <mjbroungou@gmail.com>
2023-01-17 11:19:54 -0500

    complet

--- patternprinting5.py
+++ patternprinting5.py
@@ -11,3 +11,21 @@
 # * * *
 # * *
 # *
+
+for i in range(6):
+    if i <= 5:
+        print('* ' * i)
+    if i == 5:
+        for i in range(4, 0, -1):
+            print('* '*i)
+
+# another option
+rows = 5
+for i in range(0, rows):
+    for j in range(0, i+1):
+        print('*', end=' ')
+    print("\r")
+for i in range(rows, 0, -1):
+    for j in range(0, i-1):
+        print('*', end=' ')
+    print("\r")
