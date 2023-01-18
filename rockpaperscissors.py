56de08df9ee601bf7fb136d72fcabc6e4c7cf99e
Malick Roungou <mjbroungou@gmail.com>
2023-01-18 13:51:13 -0500

    new exercise

---
+++ rockpaperscissors.py
@@ -0,0 +1,3 @@
+# python implementation of the rock paper scissors game
+# the player is going to be playing against the computer
+# rules: rock beats scissors, scissors beats paper, paper beats rock
