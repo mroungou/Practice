7b3bb0bd2826f4c3c3355aeb3879c12537e0ada3
Malick Roungou <mjbroungou@gmail.com>
2023-01-19 18:41:28 -0500

    logic implementation

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,3 +1,31 @@
 # python implementation of the rock paper scissors game
 # the player is going to be playing against the computer
 # rules: rock beats scissors, scissors beats paper, paper beats rock
+
+import random
+import sys
+# player choosing their move (rock, paper or scissors)
+# putting lowercase() to input
+player_input = input("Choose Rock(R), Paper(P), or Scissors(S) ").lower()
+# computer will randomize between 1,2,3 which will be assigned to rock, paper, and scissors.
+computer_input = random.randint(1, 3)
+
+# implementiing game logic
+if computer_input == 1 and player_input == 'r':
+    print('Draw')
+elif computer_input == 1 and player_input == 'p':
+    print('You won! ')
+elif computer_input == 1 and player_input == 's':
+    print('You lost! ')
+elif computer_input == 2 and player_input == 'r':
+    print('You lost! ')
+elif computer_input == 2 and player_input == 'p':
+    print('Draw')
+elif computer_input == 2 and player_input == 's':
+    print('You won! ')
+elif computer_input == 3 and player_input == 's':
+    print('Draw')
+elif computer_input == 3 and player_input == 'p':
+    print('You lost! ')
+elif computer_input == 3 and player_input == 'r':
+    print('You won! ')
